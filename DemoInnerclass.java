b261313888dc78dfaf562fec5a8539a08199700d
Dipamjyoti <59157210+Dipamjyoti@users.noreply.github.com>
2019-12-27 17:12:04 +0530

    Add files via upload

--- DemoInnerclass.java
+++ DemoInnerclass.java
@@ -1,22 +1,18 @@
-package inheritance;
+package demo;
 
-class ChangePassword
-{
-	String password = "pass@123";
-	class EncryptPassword{
-		int passwordLevel = 5;
-		public void doEncrypt() {
-			System.out.println("The password is : "+password);
-		}
-	}
+interface ChangePassword{
+	void doChange();
 }
 
 public class DemoInnerclass {
-
 	public static void main(String[] args) {
-		ChangePassword pa = new ChangePassword();
-		ChangePassword.EncryptPassword e = pa.new EncryptPassword();
-		e.doEncrypt();
+		ChangePassword c = new ChangePassword() {
+			
+			@Override
+			public void doChange() {
+				System.out.println("Password changed successfully!!!");				
+			}
+		};
+		c.doChange();
 	}
-
 }
